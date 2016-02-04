e06de63198c88cb581485d96f4540ec525fce2e9
jyoona <sk333jpn@hotmail.co.jp>
2016-02-03 23:13:45 -0800

    First commit

---
+++ VancouverWaterFountains/HubPage.xaml.cs
@@ -0,0 +1,146 @@
+﻿using VancouverWaterFountains.Common;
+using VancouverWaterFountains.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Bing.Maps;
+using Windows.Devices.Geolocation;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI;
+
+// The Hub Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=321224
+
+namespace VancouverWaterFountains
+{
+
+    /// <summary>
+    /// A page that displays a grouped collection of items.
+    /// </summary>
+    public sealed partial class HubPage : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        private Geolocator geolocator = null;
+        private CancellationTokenSource _cts = null;
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        public HubPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+
+            geolocator = new Geolocator();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private async void navigationHelper_LoadState(object sender, LoadStateEventArgs e) {}
+
+        /// <summary>
+        /// Invoked when a HubSection header is clicked.
+        /// </summary>
+        /// <param name="sender">The Hub that contains the HubSection whose header was clicked.</param>
+        /// <param name="e">Event data that describes how the click was initiated.</param>
+        void Hub_SectionHeaderClick(object sender, HubSectionHeaderClickEventArgs e) {
+            HubSection section = e.Section;
+            var group = section.DataContext;
+            this.Frame.Navigate(typeof(SectionPage), ((WaterFountain)group).name);
+        }
+
+        /// <summary>
+        /// Invoked when an item within a section is clicked.
+        /// </summary>
+        /// <param name="sender">The GridView or ListView
+        /// displaying the item clicked.</param>
+        /// <param name="e">Event data that describes the item clicked.</param>
+        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+           // leave it
+        }
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+           
+
+            navigationHelper.OnNavigatedTo(e);
+            var sampleDataGroup = await SampleDataSource.GetGroupAsync("");
+
+            foreach (WaterFountain w in sampleDataGroup)
+            {
+                //this.DefaultViewModel["Section3Items"] = sampleDataGroup;
+
+                _cts = new CancellationTokenSource();
+                CancellationToken token = _cts.Token;
+                geolocator.DesiredAccuracy = Windows.Devices.Geolocation.PositionAccuracy.High;
+                Geoposition pos = await geolocator.GetGeopositionAsync();  
+                Location location = new Location(pos.Coordinate.Point.Position.Latitude, pos.Coordinate.Point.Position.Longitude);
+                Pushpin gps = new Pushpin();
+                gps.Background = new SolidColorBrush(Colors.Red);
+                MapLayer.SetPosition(gps, new Location(location.Latitude, location.Longitude));
+                map.Children.Add(gps);
+
+                Pushpin pin = new Pushpin();
+                MapLayer.SetPosition(pin, new Location(w.coordinates_lon, w.coordinates_lat));
+                map.Children.Add(pin);
+                ToolTipService.SetToolTip(pin, "Pushpin 1\r\nThis is the first pushpin infobox");
+            }
+        }
+
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+    }
+}
